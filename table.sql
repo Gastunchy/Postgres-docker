9e82ab0297e521863282937f43acf7929e15c4ff
Gastunchy <97688245+Gastunchy@users.noreply.github.com>
2023-10-18 12:50:31 -0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE personas (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(255),
-    apellido VARCHAR(255),
-    Marta VARCHAR(255),
+    Nombre VARCHAR(255),
+    Apellido VARCHAR(255),
+    Estado_civil VARCHAR(255),
     Ciudad VARCHAR(255),
     Pais VARCHAR(255),
     fecha_nacimiento DATE
